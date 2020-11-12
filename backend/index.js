48dc09db8552cc9d3cd8d1cf814d99837e9c3014
greszter <esztergreskovics@gmail.com>
2020-11-12 16:19:13 +0100

    add backend for creating, updating, deleting, liking posts

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,20 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
+import postRoutes from './routes/post.routes.js';
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-const CONNECTION_URL ='';
+app.use('/posts', postRoutes);
+
+const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
